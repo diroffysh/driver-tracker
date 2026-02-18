ab9c063db03eef5f7870b16743e09805e48ad0fd
diroffysh <diroff@yenepoyahospital.com>
2026-02-18 10:54:23 +0530

    Add files via upload
    added pwa version

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'driver-tracker-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/driver-tracker/driver-tracker-pwa.html',
+        '/driver-tracker/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
